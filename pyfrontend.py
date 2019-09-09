0e30f21bb274fbcc5865a31bd7111e3b337d6f94
Yxbcvn410 <a.r.yavorskiy@gmail.com>
2019-09-09 15:44:17 +0300

    pyfrontend.py now works correctly

--- pyfrontend.py
+++ pyfrontend.py
@@ -1,15 +1,29 @@
 import sys
 import subprocess
 
+req_args = ['lambda', 'start', 'end', 'step', 'threads', 'points']
 buff = ""
 args = {}
 for arg in sys.argv:
     argsp = arg.split(sep = "=", maxsplit = 2)
     if len(argsp) == 2:
         args.update({arg.split(sep = "=", maxsplit = 2)[0].replace("-", "") : arg.split(sep = "=",maxsplit = 2)[1]})
-if len(args) != 6:
-    print("ERROR: Too {} arguments. Terminated.".format({True: "many", False: "few"}[len(args)>6]))
+        
+if len(args) != len(req_args):
+    print("ERROR: Too {} arguments.\nRequired arguments:".format({True: "many", False: "few"}[len(args)>6]))
+    for a in req_args:
+        print(a)
+elif not set(args.keys()).issubset(set(req_args)):
+    for ab in req_args:
+        if list(args.keys()).count(ab)==0:
+            print("Error: Missing argument definition for {}".format(ab))
 else:
-    a = PIPE;
-    a.write(args['start'])
-    subprocess.run("./MARS_2", stdin=a)
+    proc = subprocess.Popen(['./MARS_2'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+    for req_arg in req_args:
+        print(proc.stdout.readline(), end="")
+        proc.stdin.write(args[req_arg]+'\n')
+        proc.stdin.flush()
+    s = proc.stdout.readline()
+    while s != '':
+        print(s, end="")
+        s = proc.stdout.readline()
